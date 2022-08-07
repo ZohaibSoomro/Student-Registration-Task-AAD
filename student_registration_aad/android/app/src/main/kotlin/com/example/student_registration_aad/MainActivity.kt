853716f3cd0b4144cda33fbd0939371d027e810f
zohaibsoomro006 <zohaibsoomro006@gmail.com>
2022-08-07 16:14:08 +0500

    Student Registration Project

---
+++ student_registration_aad/android/app/src/main/kotlin/com/example/student_registration_aad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.student_registration_aad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ student_registration_aad/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:student_registration_aad/students_info/students_info_provider.dart';
+
+import 'pages/signup_or_login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => StudentInfoProvider()),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SignupOrLogin(),
+      ),
+    );
+  }
+}
